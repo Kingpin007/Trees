ce626643e098d8c0d787a8d890456dcb02ca6803
Kingpin007 <anirudhkanabar@gmail.com>
2015-12-21 17:39:32 +0530

    Added findHeight(), Traversal functions, findMin() & findMax()
    Traversal functions include
    levelOrder()
    preOrder()
    inOrder()
    postOrder()

--- Binary-Search-Tree/main.cpp
+++ Binary-Search-Tree/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -10,6 +11,13 @@ struct Node
 
 Node* insert(Node*,int);
 Node* createNewNode(int);
+int findMin(Node*);
+int findMax(Node*);
+int findHeight(Node*);
+void levelOrder(Node*);
+void inOrder(Node*);
+void preOrder(Node*);
+void postOrder(Node*);
 bool search(Node*,int);
 
 int main()
@@ -25,6 +33,19 @@ int main()
     cout<<"Found!\n";
   else
     cout<<"Not Found!\n";
+  cout<<"Minimum is: "<<findMin(root)<<"\nMaximum is: "<<findMax(root);
+  cout<<"\nLevelOrder output: ";
+  levelOrder(root);
+  cout<<endl;
+  cout<<"\nPreOder output: ";
+  preOrder(root);
+  cout<<endl;
+  cout<<"\nInOrder output: ";
+  inOrder(root);
+  cout<<endl;
+  cout<<"\nPostOrder output: ";
+  postOrder(root);
+  cout<<endl;
   return 0;
 }
 
@@ -58,4 +79,82 @@ bool search(Node* root, int x)
     search(root->right,x);
   else
     return true;
+}
+
+int findMin(Node *root)
+{
+  if(root == NULL)
+  {
+    cout<<"Error, the tree is empty!\n";
+    return -1;
+  }
+  else if(root->left==NULL)
+    return root->data;
+  return findMin(root->left);
+}
+
+int findMax(Node *root)
+{
+  if(root == NULL)
+  {
+    cout<<"Error, the tree is empty!\n";
+    return -1;
+  }
+  else if(root->right==NULL)
+    return root->data;
+  return findMax(root->right);
+}
+
+int findHeight(Node *root)
+{
+  if(root == NULL)
+    return -1;
+  int l=findHeight(root->left);
+  int r=findHeight(root->right);
+  return ((l>r?l:r)+1);
+}
+
+void levelOrder(Node *root)
+{
+  if(root == NULL)
+    return;
+  queue<Node*> q;
+  q.push(root);
+  while(!q.empty())
+  {
+    Node *current = q.front();
+    cout<<current->data<<"->";
+    if(current->left!=NULL)
+      q.push(current->left);
+    if(current->right!=NULL)
+      q.push(current->right);
+    q.pop();
+  }
+}
+
+void preOrder(Node *root)
+{
+  if(root == NULL)
+    return;
+  cout<<root->data<<"->";
+  preOrder(root->left);
+  preOrder(root->right);
+}
+
+void inOrder(Node *root)
+{
+  if(root == NULL)
+    return;
+  inOrder(root->left);
+  cout<<root->data<<"->";
+  inOrder(root->right);
+}
+
+void postOrder(Node *root)
+{
+  if(root == NULL)
+    return;
+  postOrder(root->left);
+  postOrder(root->right);
+  cout<<root->data<<"->";
 }
